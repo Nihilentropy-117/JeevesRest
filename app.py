e9d71fef4bdd5a144ad89eae713a6727c0691d4d
Gray Lott <gray@luxon.dev>
2022-12-08 23:49:59 -0500

    Add Reqs

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, request
 import os
+import mailActions
 
 app = Flask(__name__)
 
-username = os.environ['MY_USER']
-password = os.environ['MY_PASS']
+credentials = {
+    "username": os.environ['MY_USER'],
+    "password": os.environ['MY_PASS'],
+    "imap": 'imap.gmail.com',
+    "smtp": "smtp.gmail.com"
+}
 
 
-@app.route('/user')
-def get_incomes():
-    incomes = ("Running with user: %s" % username)
-    return incomes
-
-
+@app.route('/run')
+def run():
+    command = request.args.get('command', default="*", type=str)
+    mailActions.sendMail(credentials, "gray.lott@gmail.com", "test", command)
+    print("Mail Found, Processing")
